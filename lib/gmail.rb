979eb53c499c5507c9a206d8e7144adcfb3971e7
ore <orenoimac@gmail.com>
2016-09-29 11:34:08 +0900

    fixes Net::IMAP::BadResponseError: Could not parse command

--- lib/gmail.rb
+++ lib/gmail.rb
@@ -11,7 +11,7 @@ end
 class Object
   def to_imap_date
     date = respond_to?(:utc) ? utc.to_s : to_s
-    Date.parse(date).strftime("%d-%B-%Y")
+    Date.parse(date).strftime("%d-%b-%Y")
   end
 end
 
